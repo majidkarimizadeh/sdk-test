75bff7c5570c0e2272ed7f4e2b34bc7b2c32cc7a
Majid Karimizadeh <m.karimizadeh@global.leaseweb.com>
2024-12-20 13:49:23 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/majidkarimizadeh/sdk-test
+
+go 1.22.9
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "github.com/leaseweb/leaseweb-go-sdk/abuse"
+	_ "github.com/leaseweb/leaseweb-go-sdk/aggregationpack"
+	_ "github.com/leaseweb/leaseweb-go-sdk/dedicatedserver"
+	_ "github.com/leaseweb/leaseweb-go-sdk/dns"
+	_ "github.com/leaseweb/leaseweb-go-sdk/invoice"
+	_ "github.com/leaseweb/leaseweb-go-sdk/publiccloud"
+)
+
+func main() {
+}
